7e34a64fbc8885063df8bc251c9825ad740d5c5c
Pedro Henrique <pedrooheenrique159@gmail.com>
2021-06-04 16:07:23 -0300

    Initial Commit

---
+++ DesignPatternsl/ICCC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsl
+{
+    public class ICCC : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            switch (orcamento.Valor)
+            {
+                case <1000:
+                    return orcamento.Valor * 0.05;
+                case <=3000:
+                    return orcamento.Valor * 0.07;
+                case >3000:
+                    return orcamento.Valor * 0.08 + 30;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ DesignPatternsl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatternsl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imposto iss = new Iss();
+            Imposto icms = new Icms();
+            Imposto iccc = new ICCC();
+
+            Orcamento orcamento = new Orcamento(3100);
+
+            CalculadorDeImposto calculador = new CalculadorDeImposto();
+            calculador.RealizaCalculo(orcamento, iss);
+            calculador.RealizaCalculo(orcamento, icms);
+            calculador.RealizaCalculo(orcamento, iccc);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
